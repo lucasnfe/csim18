b0d6f5b955ac69e358aada2c75622b7c348a3642
Lucas N. Ferreira <lucasnfe@gmail.com>
2018-06-26 01:27:20 -0700

    lec1: intro. to LOVE2D.

---
+++ src/lec1/main.lua
@@ -0,0 +1,45 @@
+--[[
+    CSIM 2018
+    Lecture 1
+
+    -- Main Program --
+    Author: Lucas N. Ferreira
+    lferreira@ucsc.edu
+]]
+
+-- Loading external libraries
+local sti = require "lib.sti"
+local push = require "lib.push"
+
+-- Setting values of global variables
+local gameWidth, gameHeight = 256, 256
+local windowWidth, windowHeight, flags = love.window.getMode()
+
+function love.load()
+	-- Set love's default filter to "nearest-neighbor".
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	-- Initialize virtual resolution
+	push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = false})
+
+	-- Load map created with Tiled and exported to lua format
+	map = sti("map/lec1.lua", {})
+
+	-- Load sound effects
+	sounds = {
+		['track1'] = love.audio.newSource('sounds/lec1.wav', 'static')
+	}
+
+	-- Play soundtrack in loop
+	sounds['track1']:setLooping(true)
+	sounds['track1']:play()
+end
+
+function love.draw()
+	push:start()
+
+	-- Draw map using sti library
+	map:draw(0, 0)
+
+	push:finish()
+end
