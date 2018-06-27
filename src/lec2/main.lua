de4e16ea3c4f0c50896c2b0d00e29a24d385d5d1
Lucas N. Ferreira <lucasnfe@gmail.com>
2018-06-27 12:12:53 -0700

    Update main with the code developed in the class.

--- src/lec2/main.lua
+++ src/lec2/main.lua
@@ -15,7 +15,15 @@ local push = require "lib.push"
 local gameWidth, gameHeight = 128, 128
 local windowWidth, windowHeight, flags = love.window.getMode()
 
+function distance(x1, y1, x2, y2)
+	return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+end
+
 function love.load()
+
+	player_x = gameWidth/2
+ 	player_y = gameHeight/2
+
 	-- Set love's default filter to "nearest-neighbor".
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -26,61 +34,60 @@ function love.load()
 	-- Initialize virtual resolution
 	push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = false})
 
-	-- Load player as a table
-	player = {}
-	player.pos_x = gameWidth/2
-	player.pos_y = gameHeight/2
-	player.speed = 25
-	player.spr = love.graphics.newImage("sprites/lec2-player.png")
+	-- Load player sprite
+	player = love.graphics.newImage("sprites/lec2-player.png")
 
-	-- Load coins
+	-- Load a coin
+	local coins_amount = 10
 	coins = {}
 
-	for i=1,5 do
-		coin = {}
-		coin.pos_x = 30 + i * 10
-		coin.pos_y = 50
-		coin.spr = love.graphics.newImage("sprites/lec2-coin.png")
-		table.insert(coins, coin)
+	for i=1,coins_amount do
+		c = {}
+		c.x = 10 + 10 * i
+		c.y = 50
+		c.spr = love.graphics.newImage("sprites/lec2-coin.png")
+		table.insert(coins, c)
 	end
 
-	-- Load sound effects
-	sounds = {}
-	sounds['player-step'] = love.audio.newSource('sounds/lec2-step.wav', 'static')
+	-- Load step sound
+	step_sfx = love.audio.newSource("sounds/lec2-step.wav", "static")
 
 	-- Load map
 	map = sti("map/lec2.lua")
 end
 
 function love.update(dt)
-	-- Move player with keyboard
-	if love.keyboard.isDown('left') then
-		player.pos_x = player.pos_x - player.speed * dt
-		love.audio.play(sounds['player-step'])
-	elseif love.keyboard.isDown('right') then
-		player.pos_x = player.pos_x + player.speed * dt
-		love.audio.play(sounds['player-step'])
-	elseif love.keyboard.isDown('up') then
-		player.pos_y = player.pos_y - player.speed * dt
-		love.audio.play(sounds['player-step'])
-	elseif love.keyboard.isDown('down') then
-		player.pos_y = player.pos_y + player.speed * dt
-		love.audio.play(sounds['player-step'])
+	-- Move on x axis
+	if (love.keyboard.isDown('left')) then
+		player_x = player_x - 2
+		love.audio.play(step_sfx)
+	elseif(love.keyboard.isDown('right')) then
+		player_x = player_x + 2
+		love.audio.play(step_sfx)
+	end
+
+	-- Move on y axis
+	if (love.keyboard.isDown('up')) then
+		player_y = player_y - 2
+		love.audio.play(step_sfx)
+	elseif(love.keyboard.isDown('down')) then
+		player_y = player_y + 2
+		love.audio.play(step_sfx)
 	end
 end
 
 function love.draw()
 	push:start()
 
-	-- Draw map using sti library
+	-- Draw map
 	map:draw()
 
-	-- Draw player
-	love.graphics.draw(player.spr, player.pos_x, player.pos_y)
+	-- Draw the player sprite
+	love.graphics.draw(player, player_x, player_y)
 
-	-- Draw all coins
+	-- Draw coins
 	for i=1,#coins do
-		love.graphics.draw(coins[i].spr, coins[i].pos_x, coins[i].pos_y)
+		love.graphics.draw(coins[i].spr, coins[i].x, coins[i].y)
 	end
 
 	push:finish()
