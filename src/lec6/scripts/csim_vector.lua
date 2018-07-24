0d9aaa192ab3b94f29499468d810f2d2c95463c4
Lucas N. Ferreira <lucasnfe@gmail.com>
2018-07-24 10:39:37 -0700

    Add vector library.

---
+++ src/lec6/scripts/csim_vector.lua
@@ -0,0 +1,15 @@
+--[[
+    CSIM 2018
+
+    Author: Lucas N. Ferreira
+    lferreira@ucsc.edu
+]]
+
+local csim_vector = class()
+
+function csim_vector:init(x, y)
+    self.x = x
+    self.y = y
+end
+
+return csim_vector
