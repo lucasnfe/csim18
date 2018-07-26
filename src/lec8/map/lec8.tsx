f2f17144a36b244ca1656a1e81943dd0c836d91d
Lucas N. Ferreira <lucasnfe@gmail.com>
2018-07-26 00:28:05 -0700

    Coins on the map

--- src/lec8/map/lec8.tsx
+++ src/lec8/map/lec8.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="terrain" tilewidth="8" tileheight="8" tilecount="2" columns="1">
+<tileset name="terrain" tilewidth="8" tileheight="8" tilecount="4" columns="1">
  <image source="lec8.png" trans="ff00ff" width="8" height="16"/>
 </tileset>
